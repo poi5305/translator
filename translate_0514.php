a739ea5657f65c76c1e885bcea8266f35d0d090d
Andy <poi5305@gmail.com>
2014-02-16 21:09:18 +0800

    add customer dict function

--- translate_0514.php
+++ translate_0514.php
@@ -2,6 +2,7 @@
 ini_set("user_agent", "Mozilla/5.0 (Windows;+U; Windows+NT+5.2; zh-CN; rv:1.9.0.10) Gecko/2009042316 Firefox/3.0.10");
 function c2e($S){
   $S = str_ireplace(" ","%20",$S);
+  if($R = get_dict($S))	return $R;
   $S = iconv("UTF-8","big5",$S);
   $url = "http://translate.google.com.tw/translate_a/t?client=t&text=".$S."&hl=zh-TW&sl=zh-CN&tl=en&multires=1&otf=2&ssel=0&tsel=0&sc=1";
   $R = get($url);
@@ -9,12 +10,30 @@ function c2e($S){
 }
 function e2c($S){
   $S = str_ireplace(" ","%20",$S);
+  if($R = get_dict($S))	return $R;
   $S = iconv("UTF-8","big5",$S);
   $url = "http://translate.google.com.tw/translate_a/t?client=t&text=".$S."&hl=zh-TW&sl=en&tl=zh-TW&multires=1&prev=conf&psl=auto&ptl=zh-CN&otf=1&rom=1&it=sel.5439%2Ctgtd.1587&ssel=4&tsel=4&uptl=zh-TW&sc=1";
   $R = get($url);
   return $R;
 }
+function get_dict($S)
+{
+  if(is_file("dict.txt"))
+  {
+	  $dict = File("dict.txt");
+	  foreach($dict as $line)
+	  {
+		  $line = explode("\t", $line);
+		  if(strtolower($line[0]) == strtolower($S))
+		  {
+			  return $line[1];
+		  }
+	  }
+  }
+  return 0;
+}
 function get($url){
+  
   $R = file_get_contents($url);
   $R = str_replace("[","",$R);
   $R = str_replace("]","",$R);
